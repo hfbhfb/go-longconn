adb96eb8db2807a972c24774f574158baebe42ce
hfbhfb <hefabao@126.com>
2024-06-15 18:07:53 +0800

    提交

---
+++ README.md
@@ -0,0 +1,32 @@
+
+
+
+
+
+# 测试
+curl localhost/long/16
+curl localhost/long/32
+curl localhost
+
+
+# 构建
+
+
+```sh
+
+cd src
+
+Regin=swr.cn-north-4.myhuaweicloud.com
+Org=hfbbg4
+AppName=longconn
+Version=v0.1
+
+docker build --build-arg TARGETARCH=amd64 --platform linux/amd64 -t ${Regin}/${Org}/${AppName}:${Version} --no-cache .
+
+docker push ${Regin}/${Org}/${AppName}:${Version}
+
+
+
+```
+
+
---
+++ src/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func Fnlong(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the content type to text/event-stream
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	i := 0
+	count, _ := strconv.Atoi(name)
+	for {
+		i++
+		fmt.Fprintf(w, "all: %v  . current: %v\n", count, i)
+		flusher.Flush() // Flush the buffer to the client
+		if i > count {
+			break
+
+		}
+		time.Sleep(time.Second)
+
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HelloHandler)
+	r.HandleFunc("/long/{name}", Fnlong).Methods("GET")
+	r.HandleFunc("/longconn/{name}", Fnlong).Methods("GET")
+
+	http.Handle("/", r)
+	fmt.Println("Starting server on :80")
+	http.ListenAndServe(":80", nil)
+}
